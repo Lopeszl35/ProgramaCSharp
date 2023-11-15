439f8197202661bd94be9933cbca42f3ec1bbd95
rafael lopes <rafalopes900@hotmail.com>
2023-11-15 17:31:38 -0300

    Convertendo a string informada pelo usuário e mostrando a tabuada desse número

--- ProgramaCSharp/Program.cs
+++ ProgramaCSharp/Program.cs
@@ -10,6 +10,23 @@ namespace ProgramaCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Escrevendo a tabuada");
+            Console.WriteLine("Digite o número que deseja a tabuada: ");
+            string entrada = Console.ReadLine();
+
+            if(int.TryParse(entrada, out int num))
+            {
+                for(int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(num + " x " + i + " = " + (num * i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Número informado inválido");
+            }
+
+            Console.ReadKey();
         }
     }
 }
